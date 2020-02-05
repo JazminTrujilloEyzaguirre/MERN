ccff86873ba747988e4746a5510d1c166bbfbcb6
jaz-cpu <jazmin.jahazieltru@gmail.com>
2020-02-05 00:24:46 -0300

    server

---
+++ src/index.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const morgan = require('morgan');
+const path = require('path');
+ 
+const app = express();
+
+// Setting
+app.set('port', process.env.PORT  || 3000);
+
+//middlewares
+app.use(morgan('dev'));
+app.use(express.json());
+
+//routes
+app.use('/api/task', require('./routes/task.routes'));
+
+//static files
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+// starting the server
+
+
+app.listen(app.get('port'), () => {
+    console.log(`server on port ${app.get('port')}`);
+});
